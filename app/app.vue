545e102b3e5e31e934eb37eecc3cd3f3d09a3ae0
Lucas Raffarin <contact@lucasraffarin.fr>
2026-03-02 15:42:10 +0100

    init

---
+++ app/app.vue
@@ -0,0 +1,57 @@
+<script setup>
+import LRicon from '@/assets/icons/FavIcon.svg'
+
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.svg' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'QRTravel - LucasRaffarin.fr'
+const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <UApp>
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <UFooter>
+      <template #left>
+        <a href="https://lucasraffarin.fr" class="text-sm text-muted">
+          LucasRaffarin.fr
+        </a>
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+
+        <UButton
+          to="https://github.com/LucasRaffarin/QrTravel"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+          color="neutral"
+          variant="ghost"
+        />
+      </template>
+    </UFooter>
+  </UApp>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<!-- <script>
+import Luggage from "./assets/icons/facicon.svg"
+</script> -->
+
+<template>
+  <section class="bgimg h-[calc(100dvh-2rem)] m-4 p-4 rounded-md">
+    <div class="flex flex-col justify-center items-center text-center gap-5">
+      <div class="flex">
+        <Luggage />
+        <h1 class="text-4xl">QRTravel</h1>
+      </div>
+      <p class="max-w-3xl">QRTravel génère des QRCodes pour les voyageurs. Celui-ci est à coller sur son sac à dos et en cas de perte, si une personne bien intentionnée trouve le sac et scanne le qrcode, il peut avertir la personne qu'il a été retrouvé.</p>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-15 px-15">
+      <div class="bg-white text-black rounded-md p-4">
+        <h2>Informations</h2>
+      </div>
+      <div class="bg-white text-black rounded-md p-4">
+        <h2>QRTravel</h2>
+      </div>
+    </div>
+  </section>
+</template>
